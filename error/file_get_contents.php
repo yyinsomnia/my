624298e857051ccbd6dc8c06106e29bc48ffa3cf
yyinsomnia <yyinsomnia@gmail.com>
2014-08-15 20:33:13 +0800

    pu

---
+++ error/file_get_contents.php
@@ -0,0 +1,5 @@
+<?php
+
+for ($i = 0; $i < 5; $i++)
+	file_get_contents('http://www.iautos.cn/123');
+echo $i;
--- memcache/max_data.php
+++ memcache/max_data.php
@@ -1,25 +1,23 @@
 <?php
 
-
 $letters = range( 'a', 'z' );
 $a = '';
-while (strlen($a) < 1024*4)
-	$a .= 'a';
+while (strlen($a) < 1024*1024*10 )
+	$a .= $letters[mt_rand(0, 25)];
 var_dump(strlen($a));
 
 $memcache_obj = memcache_connect('localhost', 11211);
 
 
-memcache_set($memcache_obj, 'key', $a, false, 30);
+memcache_set($memcache_obj, 'key', $a, 0, 30);
 
 $b = memcache_get($memcache_obj, 'key');
 var_dump(strlen($b));
 
 
 
-
-
-$a = file_get_contents('houzi.txt');
+/*
+$a = file_get_contents('paopao.gif');
 var_dump(strlen($a));
 $memcache_obj = memcache_connect('localhost', 11211);
 
@@ -28,3 +26,4 @@ memcache_set($memcache_obj, 'key', $a, 0, 30);
 
 $b = memcache_get($memcache_obj, 'key');
 var_dump(strlen($b));
+*/
